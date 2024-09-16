bcec119a48090416b06efa3832fb237d70edb69b
Monique Azevedo <monique.azevedo@grupoboticario.com.br>
2024-09-15 23:04:59 -0300

    feat: criação do template e header

---
+++ src/components/Container/index.ts
@@ -0,0 +1,16 @@
+import { styled, css } from '@mui/material/styles'
+import { Container as ContainerMUI } from '@mui/material'
+
+export const Container = styled(ContainerMUI)`
+  ${({ theme }) => css`
+    max-width: 100%;
+    padding: 24px;
+    display: flex;
+    gap: 24px;
+
+    @media (min-width: ${theme.breakpoints.values.sm}${theme.breakpoints.unit}) {
+      max-width: 1200px;
+      margin: 0 auto;
+    }
+  `}
+`
