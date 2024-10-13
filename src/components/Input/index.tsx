c726852c5b45c6471cd8fa94ab88377459123d71
Monique Azevedo <monique.azevedo@grupoboticario.com.br>
2024-10-13 20:16:43 -0300

    feat: adiciona callback no componente input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -18,15 +18,24 @@ type InputProps = {
    * Status do input
    */
   error?: boolean
+  /**
+   * Função de callback
+   */
+  callback?: (value: string) => void
 }
 
-export const Input = ({ placeholder, label, message, error = false }: InputProps) => {
+export const Input = ({ placeholder, label, message, error = false, callback }: InputProps) => {
   const status = error ? 'error' : undefined
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    callback && callback(event.target.value)
+  }
+
   return (
     <S.Container>
       {label && <S.Label htmlFor='input'>{label}</S.Label>}
       <FormControl fullWidth variant='outlined' color={status} focused={!!status}>
-        <S.Input id='input' placeholder={placeholder} />
+        <S.Input id='input' placeholder={placeholder} onChange={onChange} />
         <S.Message id='message' error={status === 'error'}>
           {message}
         </S.Message>
