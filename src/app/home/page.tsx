5e3471067cc097234bbc9c2479c655d102258975
Monique Azevedo <monique.azevedo@grupoboticario.com.br>
2024-10-15 22:11:17 -0300

    feat: adiciona teste de chamada a api

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,34 @@
 'use client'
+import { useEffect } from 'react'
 
 import BaseHome from '@/templates/BaseHome'
 
+type User = {
+  id: number
+  name: string
+}
+
 export default function Home() {
+  useEffect(() => {
+    async function getUsers() {
+      try {
+        const res = await fetch('/api/users')
+        if (!res.ok) {
+          throw new Error('Failed to fetch users')
+        }
+        const data: User[] = await res.json()
+        console.log('data: ', data)
+      } catch (error) {
+        console.log('error: ', error)
+      }
+    }
+
+    getUsers()
+  }, [])
+
   return (
     <BaseHome>
-    <p>OI</p>
+      <p>OI</p>
     </BaseHome>
   )
 }
