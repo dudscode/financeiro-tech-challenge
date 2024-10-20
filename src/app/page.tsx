55df0d4de71603f43b71d93a31de9c0266483e14
Fernanda Kuhn <fernandakuhn1@hotmail.com>
2024-10-20 19:09:46 -0300

    feat(home): adicionar conteúdo da página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,11 @@ import Base from '@/templates/Base'
 import { BalanceCard } from '@/components/BalanceCard'
 import { TransactionCard } from '@/components/TransactionCard'
 
+
 export default function Home() {
   return (
     <Base>
-      <BalanceCard name='Joana' date='Quinta-feira, 08/09/2022' balance={2500.0} />
+     <BalanceCard name='Joana' date='Quinta-feira, 08/09/2022' balance={2500.0} />
       <TransactionCard
         onTransactionSubmit={function (type: 'deposit' | 'transfer', amount: number): void {
           throw new Error('Function not implemented.')
