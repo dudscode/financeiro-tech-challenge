6d0582267678b9ab327f71c4c99131af76a83ba0
Lucas Coutinho <lucascgs1@hotmail.com>
2024-10-20 23:11:59 -0300

    fix: importar greycard  componente de investimentos e transacoes

--- src/components/InvestmentsCard/index.tsx
+++ src/components/InvestmentsCard/index.tsx
@@ -1,91 +1,101 @@
 import React from 'react'
 import * as S from './styles'
-import { PieChart } from '@mui/x-charts/PieChart';
-import Box from '@mui/material/Box';
-import useIsMobile from '@/hooks/useIsMobile';
-import useIsTablet from '@/hooks/useIsTablet';
+import { PieChart } from '@mui/x-charts/PieChart'
+import useIsMobile from '@/hooks/useIsMobile'
+import useIsTablet from '@/hooks/useIsTablet'
+import GreyCard from '../CardGrey'
 
-export const InvestmentsCard = ({ title = "Investimentos", total = '50.000.00', rendaVariavel = '14.000,00', rendaFixa = '36.000,00' }) => {
-    const isMobile = useIsMobile();
-    const isTablet = useIsTablet();
-    const sizeChartContainer = sizeChart(isMobile, isTablet);
-    return (
-        <S.Container>
-            <S.Title variant='h2'>{title}</S.Title>
-            <S.Paragraph txt='25px' type='valor' mb="32px">Total: R$ {total} </S.Paragraph>
-            <S.ContainerRenda>
-                <S.renda><S.Paragraph txt='16px' type='info'>Renda Fixa </S.Paragraph>
-                    <S.Paragraph txt='20px'>R$ {rendaFixa} </S.Paragraph>
-                </S.renda>
-                <S.renda>
-                    <S.Paragraph txt='16px' type='info'>Renda variável </S.Paragraph>
-                    <S.Paragraph txt='20px'>R$ {rendaVariavel} </S.Paragraph>
-                </S.renda>
-            </S.ContainerRenda>
-            <S.Paragraph txt='20px' type='titulo' mb="32px">Estatísticas</S.Paragraph>
-            <S.ContainerEstatistica>
-                <S.BoxGrafico>
-                    <PieChart
-                        series={[
-                            {
-                                data: [
-                                    { id: 3, value: 25, label: 'Fundos de investimento', color: 'rgba(37, 103, 249, 1)' },
-                                    { id: 2, value: 25, label: 'Tesouro Direto', color: 'rgba(143, 60, 255, 1)' },
-                                    { id: 1, value: 25, label: 'Previdência Privada', color: 'rgba(255, 60, 130, 1)' },
-                                    { id: 0, value: 25, label: 'Bolsa de Valores', color: 'rgba(241, 130, 61, 1)' },
-                                ],
-                                innerRadius: 70,
-                                outerRadius: 100,
-                                paddingAngle: 0,
-                                cornerRadius: 0,
-                                startAngle: 0,
-                                endAngle: 360,
-                                 cx: 150,
-                                 cy: isMobile? 150 : '50%',
-                                
-                            },
-                        ]}
-                        width={sizeChartContainer.width}
-                        height={sizeChartContainer.height}
-                        slotProps={
-                            {
-                                legend: {
-                                    direction: 'column',
-                                    position: {
-                                        horizontal: isMobile ? 'left' : 'right',
-                                        vertical: isMobile ? 'bottom' : 'middle'
-                                    },
-                                    itemGap: 10,
-                                    markGap: 16,
-                                    labelStyle: {
-                                        fill: 'white',
-                                        fontSize: '16px',
-                                        fontFamily: 'Inter',
-                                    },
-                                    hidden: false
-                                }
-                            }
-                        }
-                    />
-                </S.BoxGrafico>
-
-            </S.ContainerEstatistica>
-        </S.Container>
-    )
+export const InvestmentsCard = ({
+  title = 'Investimentos',
+  total = '50.000.00',
+  rendaVariavel = '14.000,00',
+  rendaFixa = '36.000,00'
+}) => {
+  const isMobile = useIsMobile()
+  const isTablet = useIsTablet()
+  const sizeChartContainer = sizeChart(isMobile, isTablet)
+  return (
+    <GreyCard>
+      <S.Container>
+        <S.Title variant='h2'>{title}</S.Title>
+        <S.Paragraph txt='25px' type='valor' mb='32px'>
+          Total: R$ {total}{' '}
+        </S.Paragraph>
+        <S.ContainerRenda>
+          <S.renda>
+            <S.Paragraph txt='16px' type='info'>
+              Renda Fixa{' '}
+            </S.Paragraph>
+            <S.Paragraph txt='20px'>R$ {rendaFixa} </S.Paragraph>
+          </S.renda>
+          <S.renda>
+            <S.Paragraph txt='16px' type='info'>
+              Renda variável{' '}
+            </S.Paragraph>
+            <S.Paragraph txt='20px'>R$ {rendaVariavel} </S.Paragraph>
+          </S.renda>
+        </S.ContainerRenda>
+        <S.Paragraph txt='20px' type='titulo' mb='32px'>
+          Estatísticas
+        </S.Paragraph>
+        <S.ContainerEstatistica>
+          <S.BoxGrafico>
+            <PieChart
+              series={[
+                {
+                  data: [
+                    { id: 3, value: 25, label: 'Fundos de investimento', color: 'rgba(37, 103, 249, 1)' },
+                    { id: 2, value: 25, label: 'Tesouro Direto', color: 'rgba(143, 60, 255, 1)' },
+                    { id: 1, value: 25, label: 'Previdência Privada', color: 'rgba(255, 60, 130, 1)' },
+                    { id: 0, value: 25, label: 'Bolsa de Valores', color: 'rgba(241, 130, 61, 1)' }
+                  ],
+                  innerRadius: 70,
+                  outerRadius: 100,
+                  paddingAngle: 0,
+                  cornerRadius: 0,
+                  startAngle: 0,
+                  endAngle: 360,
+                  cx: 150,
+                  cy: isMobile ? 150 : '50%'
+                }
+              ]}
+              width={sizeChartContainer.width}
+              height={sizeChartContainer.height}
+              slotProps={{
+                legend: {
+                  direction: 'column',
+                  position: {
+                    horizontal: isMobile ? 'left' : 'right',
+                    vertical: isMobile ? 'bottom' : 'middle'
+                  },
+                  itemGap: 10,
+                  markGap: 16,
+                  labelStyle: {
+                    fill: 'white',
+                    fontSize: '16px',
+                    fontFamily: 'Inter'
+                  },
+                  hidden: false
+                }
+              }}
+            />
+          </S.BoxGrafico>
+        </S.ContainerEstatistica>
+      </S.Container>
+    </GreyCard>
+  )
 }
 
 interface ISizeChart {
-    width: number;
-    height: number;
+  width: number
+  height: number
 }
-const sizeChart = (isMobile:boolean, isTablet:boolean): ISizeChart => {
-    if (isMobile) {
-        return { width: 300, height: 400 }
-    }
-    else if(isTablet){
-        return { width: 600, height: 200 }
-    }
-    else {
-        return { width: 700, height: 200 }
-    }
+const sizeChart = (isMobile: boolean, isTablet: boolean): ISizeChart => {
+  if (isMobile) {
+    return { width: 300, height: 400 }
+  } else if (isTablet) {
+    return { width: 600, height: 200 }
+  } else {
+    return { width: 700, height: 200 }
+  }
 }
--- src/components/InvestmentsCard/styles.ts
+++ src/components/InvestmentsCard/styles.ts
@@ -3,79 +3,76 @@ import { Box, Theme, Typography } from '@mui/material'
 import theme from '../../../styles/theme'
 
 interface IParagraph {
-    /**
-     * text size
-     */
-    txt?: string
-    /**
-     * margin bottom
-     */
-    mb?: string
-    /**
-     * font weight
-     */
-    weight?: string
-    /**
-     * text type
-     */
-    type?: string
+  /**
+   * text size
+   */
+  txt?: string
+  /**
+   * margin bottom
+   */
+  mb?: string
+  /**
+   * font weight
+   */
+  weight?: string
+  /**
+   * text type
+   */
+  type?: string
 }
 export const Container = styled('section')`
-    ${({ theme }) => css`
-        width: 100%;
-        padding: 32px 40px 40px;
-        background: ${theme.palette.secondary.main};
-        border-radius: 8px 8px 8px 8px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        @media (min-width: ${theme.breakpoints.values.sm}${theme.breakpoints.unit}) {
-            align-items: flex-start;
-        }
-        
-    `}
+  ${({ theme }) => css`
+    z-index: 10;
+    width: 100%;
+    padding: 32px 40px 40px;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: ${theme.breakpoints.values.sm}${theme.breakpoints.unit}) {
+      align-items: flex-start;
+    }
+  `}
 `
 export const Title = styled(Typography)`
   margin-bottom: 32px;
 `
 
 export const ContainerRenda = styled('section')`
-    ${({ theme }) => css`
-        width: 100%;
-        margin-bottom: 40px;
-        display: flex;
-        flex-direction: column;
-        gap: 24px;
-        align-items: center;
-        @media (min-width: ${theme.breakpoints.values.sm}${theme.breakpoints.unit}) {
-        flex-direction: row;
-        }
-       
-    `}
+  ${({ theme }) => css`
+    width: 100%;
+    margin-bottom: 40px;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    align-items: center;
+    @media (min-width: ${theme.breakpoints.values.sm}${theme.breakpoints.unit}) {
+      flex-direction: row;
+    }
+  `}
 `
 export const renda = styled('div')`
-    ${({ theme }) => css`
-        width: 100%;
-        padding:  16px 32px;
-        background: ${theme.palette.primary.main};
-        border-radius: 8px 8px 8px 8px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-       
-    `}
+  ${({ theme }) => css`
+    width: 100%;
+    padding: 16px 32px;
+    background: ${theme.palette.primary.main};
+    border-radius: 8px 8px 8px 8px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `}
 `
 
 type Types = {
-    [key: string]: (theme: Theme) => string
+  [key: string]: (theme: Theme) => string
 }
 const types: Types = {
-    default: theme => theme.palette.primary.contrastText,
-    valor: theme => theme.palette.primary.main,
-    info: theme => theme.palette.text.disabled,
-    titulo: theme => theme.palette.text.primary
+  default: theme => theme.palette.primary.contrastText,
+  valor: theme => theme.palette.primary.main,
+  info: theme => theme.palette.text.disabled,
+  titulo: theme => theme.palette.text.primary
 }
-export const Paragraph = styled('p') <IParagraph>`
+export const Paragraph = styled('p')<IParagraph>`
   ${({ theme, txt = '12px', mb = 0, weight = 400, type = 'default' }) => css`
     color: ${types[type](theme)};
     font-size: ${txt};
@@ -85,25 +82,24 @@ export const Paragraph = styled('p') <IParagraph>`
 `
 
 export const ContainerEstatistica = styled('section')`
-    ${({ theme }) => css`
-        width: 100%;
-        margin-bottom: 40px;
-        padding: 32px 16px 0px;
-        border-radius: 8px 8px 8px 8px;
-        background:${theme.palette.primary.main};
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    `}
+  ${({ theme }) => css`
+    width: 100%;
+    padding: 32px 16px 0px;
+    border-radius: 8px 8px 8px 8px;
+    background: ${theme.palette.primary.main};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `}
 `
 export const legend = styled('p')`
-   ${({ theme }) => css`
-        color: ${theme.palette.primary.contrastText};
-    `}
+  ${({ theme }) => css`
+    color: ${theme.palette.primary.contrastText};
+  `}
 `
 export const BoxGrafico = styled(Box)`
-min-height: 300px;
-  width: 100%; 
-display: flex;
-  align-items: center; 
+  min-height: 300px;
+  width: 100%;
+  display: flex;
+  align-items: center;
 `
