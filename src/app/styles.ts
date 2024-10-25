ab595cd7f1e9d1ea0a039de8c6d1d9072d76092c
Fernanda Kuhn <fernandakuhn1@hotmail.com>
2024-10-25 17:56:08 -0300

    feat(home): layout responsivo

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -1,10 +1,15 @@
 import { styled, css } from '@mui/material/styles'
-import theme from '../../../styles/theme'
+import theme from '../../styles/theme'
 import { CardMedia as MuiCardMedia } from '@mui/material'
 
 export const Container = styled('div')`
-  bottom: 0;
-  background: linear-gradient(180deg, #004d61 0%, #ffffff 100%);
+   bottom: 0;
+   background: linear-gradient(180deg, #004d61 0%, #ffffff 100%);
+
+  @media (max-width: 600px) {
+      width: 100%;
+    }
+
 `
 
 export const Title = styled('div')`
@@ -15,6 +20,18 @@ export const Title = styled('div')`
     font-size: 28px;
     font-weight: 600px;
     width: 434px;
+    max-width: 100%;
+
+    @media (max-width: 1024px) {
+      text-align: center;
+      display: flex;
+      align-items: center;
+    }
+    @media (max-width: 600px) {
+     margin-top: 20px;
+    }
+
+
   `}
 `
 
@@ -27,16 +44,55 @@ export const Typography = styled('div')`
     font-size: 28px;
     font-weight: 600px;
     width: 434px;
+
+    @media (max-width: 600px) {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      font-size: 20px;
+      margin-bottom: 35px;
+    }
+
   `}
 `
 
 export const Card = styled('div')`
-  margin-top: 35px;
-  padding-bottom: 80px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  width: 282px;
+    margin-top: 35px;
+    padding-bottom: 80px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    width: 282px;
+
+   @media (max-width: 1024px) {
+     margin-left: 20% ;
+    }
+   @media (max-width: 600px) {
+    margin: 0 auto;
+    width: 100%;
+    justify-content: center;
+    }
+
 `
+
+
+export const BannerImage = styled('div')`
+   background-image: url('/images/banner-home.png');
+   background-size: contain;
+   background-position: top;
+   background-repeat: no-repeat;
+   width: 100%;
+   height: 300px;
+   margin-top: 35px;
+
+   @media (max-width: 600px) {
+    height: 200px;
+    }
+
+`;
+
+
