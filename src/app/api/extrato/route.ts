ef9b7d077dfdbccc7e27a4031f702c150b519ceb
LucasRocha2308 <lucas_rocha_25@hotmail.com>
2024-11-02 12:37:15 -0300

    feat: adjust transaction values to be negative for transfers and positive for deposits

--- src/app/api/extrato/route.ts
+++ src/app/api/extrato/route.ts
@@ -3,10 +3,26 @@ import axios from 'axios'
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://json-server-vercel-tawny-one.vercel.app'
 
+interface ExtratoItem {
+  id: string
+  mes: string
+  data: string
+  tipo: 'Transferência' | 'Depósito'
+  valor: number
+}
+
 export async function GET() {
   try {
     const response = await axios.get(`${API_URL}/extrato`)
-    return NextResponse.json(response.data)
+    const extrato: ExtratoItem[] = response.data.map((item: ExtratoItem) => {
+      if (item.tipo === 'Transferência') {
+        item.valor = -Math.abs(item.valor)
+      } else if (item.tipo === 'Depósito') {
+        item.valor = Math.abs(item.valor)
+      }
+      return item
+    })
+    return NextResponse.json(extrato)
   } catch (error) {
     console.error('Error fetching extrato data:', error)
     return NextResponse.json({ error: (error as Error).message }, { status: 500 })
@@ -15,7 +31,7 @@ export async function GET() {
 
 export async function POST(req: Request) {
   try {
-    const extrato = await req.json()
+    const extrato: ExtratoItem = await req.json()
     const response = await axios.post(`${API_URL}/extrato/`, extrato)
     return NextResponse.json(response.data)
   } catch (error) {
@@ -26,7 +42,14 @@ export async function POST(req: Request) {
 
 export async function PATCH(req: Request) {
   try {
-    const extrato = await req.json()
+    const extrato: { item: ExtratoItem } = await req.json()
+
+    if (extrato.item.tipo === 'Transferência') {
+      extrato.item.valor = -Math.abs(extrato.item.valor)
+    } else if (extrato.item.tipo === 'Depósito') {
+      extrato.item.valor = Math.abs(extrato.item.valor)
+    }
+
     const response = await axios.patch(`${API_URL}/extrato/${extrato.item.id}`, extrato.item)
     return NextResponse.json(response.data)
   } catch (error) {
@@ -37,7 +60,7 @@ export async function PATCH(req: Request) {
 
 export async function DELETE(req: Request) {
   try {
-    const extrato = await req.json()
+    const extrato: { id: string } = await req.json()
     const response = await axios.delete(`${API_URL}/extrato/${extrato.id}`)
     return NextResponse.json(response.data)
   } catch (error) {
